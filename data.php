c5793699f918b5765e90fb734fcf769ad50e5316
jet han <fajie_han@foxmail.com>
2017-04-06 15:57:24 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,8 +1,8 @@
 <?php
 
 $viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
-$title = '标题';
-$subtitle = '子标题';
+$title = 'jet han\'s resume';
+$subtitle = 'resume';
 $content = 'MarkDown';
 
 $data['local'] = 1;
